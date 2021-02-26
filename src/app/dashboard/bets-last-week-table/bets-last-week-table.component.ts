b7aeb098dcb1840b0656351dda5cab3e2d7023de
SeifertChris <chrisseifert1822@gmail.com>
2021-02-25 23:03:52 -0500

    adding last week bets table to dashboard page with fake data

--- src/app/dashboard/bets-last-week-table/bets-last-week-table.component.ts
+++ src/app/dashboard/bets-last-week-table/bets-last-week-table.component.ts
@@ -8,6 +8,68 @@ import { Component, OnInit } from '@angular/core';
 export class BetsLastWeekTableComponent {
 
 lastWeekBetsTableShowHide = false;
+lastWeekBets =[
+    {
+      "teamOne":"Eagles",
+      "teamTwo":"Dallas",
+      "overUnder":"+6",
+      "amount":"100.00",
+      "won":false,
+      "lost":true,
+      "additionalNotes":"Dallas is awesome"
+    },
+    {
+      "teamOne":"GreenBay",
+      "teamTwo":"SanFrancisco",
+      "overUnder":"-6",
+      "amount":"200.00",
+      "won":true,
+      "lost":false,
+      "additionalNotes":"Some more notes about the game"
+    },
+    {
+      "teamOne":"Carolina",
+      "teamTwo":"Falcons",
+      "overUnder":"+3",
+      "amount":"50.00",
+      "won":false,
+      "lost":true,
+      "additionalNotes":"More notes about the falcons and the fact that they suck balls and then something else happened I am just trying to get a long message to print on the screen"
+    }
+  ];
+
+// //this is going to be in its own service. It needs to be decoupled from this component for testing and best practices
+// getLastWeekBetsFromDB(){
+//     this.lastWeekBets = [
+//       {
+//       "teamOne":"Eagles",
+//       "teamTwo":"Dallas",
+//       "overUnder":"+6",
+//       "amount":"100.00",
+//       "won":false,
+//       "lost":true,
+//       "additionalNotes":"Dallas is awesome"
+//     },
+//     {
+//       "teamOne":"GreenBay",
+//       "teamTwo":"SanFrancisco",
+//       "overUnder":"-6",
+//       "amount":"200.00",
+//       "won":true,
+//       "lost":false,
+//       "additionalNotes":"Some more notes about the game"
+//     },
+//     {
+//       "teamOne":"Carolina",
+//       "teamTwo":"Falcons",
+//       "overUnder":"+3",
+//       "amount":"50.00",
+//       "won":false,
+//       "lost":true,
+//       "additionalNotes":"More notes about the falcons and the fact that they suck balls and then something else happened I am just trying to get a long message to print on the screen"
+//     }
+//   ]
+// }
 
 
 
