aeea70ef6daf8e6d0a589faac7cdf1e510aaaa3f
zachhudy <zachhudy@gmail.com>
2022-10-04 21:48:00 -0400

    Add stats-chart component

---
+++ src/app/navigation/statistics/stats-chart/stats-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-chart',
+  templateUrl: './stats-chart.component.html',
+  styleUrls: ['./stats-chart.component.scss']
+})
+export class StatsChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
