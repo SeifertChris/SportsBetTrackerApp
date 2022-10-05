e55e2a7193cbef4a1248db010719f7c98bafde5b
zachhudy <zachhudy@gmail.com>
2022-10-04 21:53:11 -0400

    Add generic chart with chart.js; add to statistics component

--- src/app/navigation/statistics/stats-chart/stats-chart.component.ts
+++ src/app/navigation/statistics/stats-chart/stats-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-stats-chart',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stats-chart.component.scss']
 })
 export class StatsChartComponent implements OnInit {
+  public chart: any;
+  createChart() {
+    this.chart = new Chart("MyChart", {
+      type: 'bar',
 
-  constructor() { }
+      data: {
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12'],
+        datasets: [
+          {
+            label: "Sales",
+            data: [467, 576, 572],
+            backgroundColor: 'blue',
+            barThickness: 6,
+            maxBarThickness: 8
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 3,
+        scales: {
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Teams'
+            }
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Score'
+            }
+          }]
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
+    this.createChart();
   }
 
 }
